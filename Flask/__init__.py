386b3d327576690245362873017a65d826f234f0
Vincentthimself <vincentyap399@gmail.com>
2025-03-30 17:18:13 +0800

    latest changes

--- Flask/__init__.py
+++ Flask/__init__.py
@@ -12,18 +12,19 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-   
+    # Import blueprints
     from .views import views
-    from .auth import auth 
+    from .auth import auth
+    # from .prediction import prediction  # Import the prediction blueprint
 
-    app.register_blueprint(views, url_prefix = '/')
-    app.register_blueprint(auth, url_prefix = '/')
-
+    # Register blueprints
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    # app.register_blueprint(prediction, url_prefix='/')  # Register the prediction blueprint
 
     from .models import User
 
     create_database(app)
-
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.register'
@@ -40,4 +41,3 @@ def create_database(app):
         with app.app_context():
             db.create_all()
             print('Created Database')
-            
