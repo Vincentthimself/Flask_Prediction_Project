41a8ed2ba6cafdbae72f58718df42884538382d0
Vincentthimself <vincentyap399@gmail.com>
2025-04-03 12:40:29 +0800

    latest changes

--- Flask/prediction.py
+++ Flask/prediction.py
@@ -1,6 +1,8 @@
+# Import Libraries
 import numpy as np 
 import pandas as pd
 import yfinance as yf
+import seaborn as sns
 import tensorflow as tf 
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import LSTM, Dense, Dropout, BatchNormalization
@@ -17,7 +19,7 @@ from flask_login import login_required, current_user
 prediction = Blueprint('prediction', __name__)
 
 # Load the model (make sure your model is in the correct path)
-model = load_model('lstm_model.h5')
+model = load_model('Flask/lstm_model.h5')
 
 @prediction.route('/prediction_page', methods=['GET', 'POST'])
 def lstm_prediction():
@@ -28,13 +30,36 @@ def lstm_prediction():
             currency_pairs = 'EURUSD=X'  # Default stock if none is entered
 
     # Download historical data
-        data = yf.download(currency_pairs, start='2020-01-01', end=datetime.datetime.today().strftime('%Y-%m-%d'))
+
+        end_date = '2025-02-28'
+        start_date = '2020-02-29'
+
+        data = yf.download(currency_pairs, start=start_date, end=end_date)
         # Focus on 'Close' prices
         data = data[['Close']].dropna()  # Drop NaN values if any
         if data.empty:
             return render_template('prediction_page.html', error="Invalid currency pair or no data available.")
-        
 
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaled_data = scaler.fit_transform(data)
+        data_desc = data.describe()   
+
+        # Exponential Moving Averages
+        ema20 = data.Close.ewm(span=20, adjust=False).mean()
+        ema50 = data.Close.ewm(span=50, adjust=False).mean()
+
+
+        # Step 2 Prepare training and testing datasets
+        training_data_len = int(np.ceil(len(data) * 0.7))
+
+        # Split the data into training and test sets
+        raw_train_data = data[:training_data_len] # 70% 
+        raw_test_data = data[training_data_len:] # 30% 
+
+
+
+
+    return render_template('home.html', 
+                           user=current_user)
+        
+        # scaler = MinMaxScaler(feature_range=(0, 1))
+        # scaled_data = scaler.fit_transform(data)
        
